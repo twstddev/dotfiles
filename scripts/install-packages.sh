a9059e1be232f694b51a37c07c38adfe900e77e5
twstddev <twstd.dev@gmail.com>
2026-01-12 11:41:18 +0000

    Add instructions for package installation on macos

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -10,6 +10,10 @@ function install_ubuntu_package() {
   sudo apt-get -qq install $1
 }
 
+function install_brew_package() {
+  brew install $1
+}
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
 
   if [[ $(lsb_release -si) == "Ubuntu" ]]; then
@@ -44,5 +48,34 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     print_installation_message "Install eza"
     install_ubuntu_package eza
   fi
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+
+    print_installation_message "Install neovim"
+    install_brew_package neovim
+
+    print_installation_message "Install tmux"
+    install_brew_package tmux
+
+    print_installation_message "Install fzf"
+    install_brew_package fzf
+
+    print_installation_message "Install htop"
+    install_brew_package htop
+
+    print_installation_message "Install ripgrep"
+    install_brew_package ripgrep
+
+    print_installation_message "Install bat"
+    install_brew_package bat
+
+    print_installation_message "Install ruby"
+    install_brew_package ruby
+
+    print_installation_message "Install httpie"
+    install_brew_package httpie
+
+    print_installation_message "Install eza"
+    install_brew_package eza
 
 fi
