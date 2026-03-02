4a1a08682daffd1e9bba1e45f9d594ee9d387aaa
twstd <twstd.dev@gmail.com>
2026-03-02 20:54:08 +0000

    Disable a few of the deafult lazyvim keybindings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,5 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
+local del = vim.keymap.del
 
 map({ "n", "v", "x", "i" }, "<C-c>", "<Esc>")
+
+del("n", "<S-h>")
+del("n", "<S-l>")
+del({ "i", "x", "n", "s" }, "<C-s>")
