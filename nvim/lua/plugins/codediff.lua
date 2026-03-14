df5410e833bab9a138892a71a79bed664d552907
twstd <twstd.dev@gmail.com>
2026-03-14 13:45:42 +0000

    Add additional codediff mappings

--- nvim/lua/plugins/codediff.lua
+++ nvim/lua/plugins/codediff.lua
@@ -1,4 +1,10 @@
 return {
   "esmuellert/codediff.nvim",
   cmd = "CodeDiff",
+  keys = {
+    { "<leader>gcd", "<cmd>CodeDiff<cr>", desc = "Show git diff" },
+    { "<leader>gch", "<cmd>CodeDiff history<cr>", desc = "Show git history" },
+    { "<leader>gcf", "<cmd>CodeDiff history %<cr>", desc = "Show git file history" },
+    { "<leader>gcl", "<cmd>'<,'>CodeDiff history<cr>", desc = "Show git line history", mode = { "n", "x" } },
+  },
 }
