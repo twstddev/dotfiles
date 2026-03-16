4079a981bc27e8fb3f19b43703219d0162cd5aa8
twstd <twstd.dev@gmail.com>
2026-03-16 21:06:49 +0000

    Remove default javascript snippets from friend-snippets

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -5,8 +5,10 @@ return {
     {
       "rafamadriz/friendly-snippets",
       config = function()
+        require("luasnip.loaders.from_vscode").lazy_load({
+          exclude = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        })
         require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
-        require("luasnip.loaders.from_vscode").lazy_load()
       end,
     },
   },
