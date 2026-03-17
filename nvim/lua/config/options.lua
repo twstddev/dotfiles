5ed91af1bfee8dbb3d2e4ceecf67650be69a696c
twstd <twstd.dev@gmail.com>
2026-03-17 20:37:26 +0000

    Add line wrapping to neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ","
+vim.opt.wrap = true
+vim.opt.breakindent = true
 -- vim.o.equalalways = false
 -- vim.g.ai_cmp = false
 --
