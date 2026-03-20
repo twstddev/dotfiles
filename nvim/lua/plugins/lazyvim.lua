49db8ec500e72e8a0d7ae202dce13cf79a88aaf4
twstd <twstd.dev@gmail.com>
2026-03-20 08:19:47 +0000

    Put antoher workaround version to get eslint working on save

--- nvim/lua/plugins/lazyvim.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "LazyVim/LazyVim",
-    -- this is a temporary workaround for the issue where eslint doesn't fix issues on save
-    commit = "16713e6e1200eb90ad4b4a394bb7254fb5612484",
-  },
-}
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -14,5 +14,34 @@ return {
     inlay_hints = {
       enabled = false,
     },
+    setup = {
+      -- this is a temporary workaround to get eslint to fix issues on save with the current version of lazyvim
+      eslint = function()
+        local formatter = LazyVim.lsp.formatter({
+          name = "eslint: EslintFixAll",
+          primary = false,
+          priority = 200,
+          filter = "eslint",
+          format = function(buf)
+            local client = vim.lsp.get_clients({ name = "eslint", bufnr = buf })[1]
+
+            if client then
+              local push_diagnostics =
+                vim.diagnostic.get(buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+              -- Older versions of the ESLint language server send push diagnostics rather than using pull.
+              -- We support both for backwards compatibility.
+              local pull_diagnostics =
+                vim.diagnostic.get(buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id, true) })
+              if (#pull_diagnostics + #push_diagnostics) > 0 then
+                vim.cmd("LspEslintFixAll")
+              end
+            end
+          end,
+        })
+
+        -- register the formatter with LazyVim
+        LazyVim.format.register(formatter)
+      end,
+    },
   },
 }
