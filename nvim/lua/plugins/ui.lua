b2ddb70cfb1c46fb71b2ffcfe26aeb9a35435fce
twstd <twstd.dev@gmail.com>
2026-04-10 21:26:11 +0100

    Display relative file path in snacks.nvim preview

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -44,6 +44,15 @@ return {
             },
           },
         },
+        preview = function(ctx)
+          local res = Snacks.picker.preview.file(ctx)
+
+          if ctx.item.file then
+            ctx.picker.preview:set_title(vim.fn.fnamemodify(ctx.item.file, ":."))
+          end
+
+          return res
+        end,
         sources = {
           explorer = {
             ignored = true,
