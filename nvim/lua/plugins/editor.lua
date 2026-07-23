61a922c9bb1e40d328b1f0601e79ce78716e165c
twstd <twstd.dev@gmail.com>
2026-07-23 21:08:08 +0100

    Tweak vim-tmux-navigator config

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -31,6 +31,9 @@ return {
 
   {
     "christoomey/vim-tmux-navigator",
+    init = function()
+      vim.g.tmux_navigator_disable_when_zoomed = 1
+    end,
     cmd = {
       "TmuxNavigateLeft",
       "TmuxNavigateDown",
