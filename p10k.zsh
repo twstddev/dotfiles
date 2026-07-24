cacd7b8ae144d9d9eb01e73a5f64cef74a12d871
twstd <twstd.dev@gmail.com>
2026-07-24 17:25:37 +0100

    Tweak powerlevel10k git status style

--- p10k.zsh
+++ p10k.zsh
@@ -361,7 +361,7 @@
 
   # Untracked files icon. It's really a question mark, your font isn't broken.
   # Change the value of this parameter to show a different icon.
-  typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON='?'
+  typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON='\ueb32 '
 
   # Formatter for Git status.
   #
@@ -452,16 +452,16 @@
     (( VCS_STATUS_PUSH_COMMITS_AHEAD && !VCS_STATUS_PUSH_COMMITS_BEHIND )) && res+=" "
     # ⇢42 if ahead of the push remote; no leading space if also behind: ⇠42⇢42.
     (( VCS_STATUS_PUSH_COMMITS_AHEAD  )) && res+="${clean}⇢${VCS_STATUS_PUSH_COMMITS_AHEAD}"
-    # *42 if have stashes.
-    (( VCS_STATUS_STASHES        )) && res+=" ${clean}*${VCS_STATUS_STASHES}"
+    # stashes (codicon archive, U+EA98).
+    (( VCS_STATUS_STASHES        )) && res+=" ${clean}${(g::)${:-\uea98 }}${VCS_STATUS_STASHES}"
     # 'merge' if the repo is in an unusual state.
     [[ -n $VCS_STATUS_ACTION     ]] && res+=" ${conflicted}${VCS_STATUS_ACTION}"
-    # ~42 if have merge conflicts.
-    (( VCS_STATUS_NUM_CONFLICTED )) && res+=" ${conflicted}~${VCS_STATUS_NUM_CONFLICTED}"
-    # +42 if have staged changes.
-    (( VCS_STATUS_NUM_STAGED     )) && res+=" ${modified}+${VCS_STATUS_NUM_STAGED}"
-    # !42 if have unstaged changes.
-    (( VCS_STATUS_NUM_UNSTAGED   )) && res+=" ${modified}!${VCS_STATUS_NUM_UNSTAGED}"
+    # merge conflicts (codicon warning, U+EA6C).
+    (( VCS_STATUS_NUM_CONFLICTED )) && res+=" ${conflicted}${(g::)${:-\uea6c }}${VCS_STATUS_NUM_CONFLICTED}"
+    # staged changes (codicon diff-added, U+EADC). Green to distinguish from unstaged.
+    (( VCS_STATUS_NUM_STAGED     )) && res+=" ${clean}${(g::)${:-\ueadc }}${VCS_STATUS_NUM_STAGED}"
+    # unstaged changes (codicon diff-modified, U+EADE).
+    (( VCS_STATUS_NUM_UNSTAGED   )) && res+=" ${modified}${(g::)${:-\ueade }}${VCS_STATUS_NUM_UNSTAGED}"
     # ?42 if have untracked files. It's really a question mark, your font isn't broken.
     # See POWERLEVEL9K_VCS_UNTRACKED_ICON above if you want to use a different icon.
     # Remove the next line if you don't want to see untracked files at all.
