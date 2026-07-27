a564299561f80f6f8b9232cf97e99d78d4a922b0
twstd <twstd.dev@gmail.com>
2026-07-27 01:06:22 +0100

    Keep tab for navigation in fzf-tab

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -53,7 +53,6 @@ zstyle ':completion:*:git-checkout:*' sort false
 zstyle ':completion:*' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' menu no
-zstyle ':fzf-tab:*' fzf-flags --bind=tab:accept
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 zstyle ':fzf-tab:*' use-fzf-default-opts yes
 zstyle ':fzf-tab:*' switch-group '<' '>'
